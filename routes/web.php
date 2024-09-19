92afd06655fb19196db111aa08a104392890fbe0
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-19 17:28:39 +0300

    added the pages

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,8 @@ Route::get('/restaurants', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
 Route::get('/contact', function () {
     return view('contact');
 });
-Route::get('/restaurants', function () {
-    return view('restaurants');
-});
+
