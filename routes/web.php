f0fba19c01dd215e678c2ddd7a54c9719f6fb97c
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-19 15:42:16 +0300

    create the web pages routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/restaurants', function () {
+    return view('restaurants');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/restaurants', function () {
+    return view('restaurants');
+});
